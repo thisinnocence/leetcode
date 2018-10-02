cd65585a7553f1fdb54ffbcec5138b184502f865
Michael <iamwuming@163.com>
2018-10-02 19:28:28 +0800

    009_PalindromeNumber.cpp, beats 91%, use 3 mins.

---
+++ algorithms/009_PalindromeNumber.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if (x < 0)
+            return false;
+
+        int y = 0;
+        int ori = x;
+        while (x) {
+            y = y * 10 + x % 10;
+            x /= 10;
+        }
+        return ori == y;
+    }
+};
